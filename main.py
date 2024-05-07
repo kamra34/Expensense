63b76d52ecbff3be5bad06a511cf08514e7ddc54
Kamiar Radnosrati <kamiar.radnosrati@ericsson.com>
2024-05-07 12:03:11 +0200

    Changing title

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title('My Multipage Streamlit App')
+st.title('My Expensense streamlit app.')
 
 # This is your main app file. Streamlit will automatically
 # load other pages from the 'pages' directory.
